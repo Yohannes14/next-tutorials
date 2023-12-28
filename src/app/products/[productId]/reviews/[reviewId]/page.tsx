7638845b6e1f501946f6739d63c8e3cf5452ea9b
Yohannes14 <yohansdegu.2010@gmail.com>
2023-12-28 17:11:49 +0300

    not found

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,4 +1,14 @@
-export default function ReviewDetails({ params }: { params: { productId: string, reviewId: string } }) {
+import { notFound } from "next/navigation"
+export default function ReviewDetails(
+    { params }: {
+        params: {
+            productId: string,
+            reviewId: string
+        }
+    }) {
+    if (parseInt(params.reviewId) > 1000) {
+        notFound()
+    }
     return (
         <h1>Review{params?.reviewId} for prduct{params?.productId}</h1>
     )
